6c2fc2197bc56cf2ac20bf32d338372a48a13834
Maks <maks35@gmail.com>
2024-12-03 23:13:10 +0200

    Array endgit  status!

--- Copy.This/Array.cpp
+++ Copy.This/Array.cpp
@@ -46,10 +46,92 @@ public:
 		std::cout << std::endl;
 	}
 
+	void fillWithRandomValues() {
+		for (size_t i = 0; i < size; ++i)
+		{
+			arr[i] = rand() % 100; 
+		}
+	}
+	void resize(size_t newSize) {
+		int* newArr = new int[newSize];
+		size_t copySize = (newSize < size) ? newSize : size;
+		for (size_t i = 0; i < copySize; ++i)
+		{
+			newArr[i] = arr[i];
+		}
+		delete[] arr;
+		arr - newArr;
+		size = newSize;
+	}
+
+	void sort() {
+		for (size_t i = 0; i < size - 1; ++i)
+		{
+			for (size_t j = 0; j < size - i - 1; ++j)
+			{
+				if (arr[j] > arr[j +1])
+				{
+					int temp = arr[j];
+					arr[j] = arr[j + 1];
+					arr[j + 1] = temp;
+				}
+			}
+
+		}
+	}
+
+	int min() const {
+		if (size == 0) throw std::runtime_error("Array is empty");
+		int minValue = arr[0];
+		for (size_t i = 0; i < size; ++i){
+			if (arr[i] < minValue)
+			{
+				minValue = arr[i];
+			}
+		}
+		
+
+		return minValue;
+	}
 	
+	int max() const {
+		if (size == 0) throw std::runtime_error("Array is empty");
+		int maxValue = arr[0];
+		for (size_t i = 0; i < size; ++i) {
+			if (arr[i] > maxValue)
+			{
+				maxValue = arr[i];
+			}
+		}
+
+
+		return maxValue;
+	}
 };
 
 int main() {
 	srand(static_cast <unsigned int>(time(NULL)));
+	
+	Array emptyArray(5);
+	emptyArray.display();
+
+	Array randomArray(10, true);
+	randomArray.display();
+
+	Array rangeArray(10, 1, 100);
+	rangeArray.display();
+
+	std::cout << "Min:" << rangeArray.min() << std::endl;
+	std::cout << "Max:" << rangeArray.max() << std::endl;
+
+	rangeArray.sort();
+	std::cout << "Sorteed array";
+	rangeArray.display();
+
+	rangeArray.resize(5);
+	std::cout << "Resized array";
+	rangeArray.display();
+
+
 	return 0;
 }
